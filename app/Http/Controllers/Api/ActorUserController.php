9e16b8bd0bdcd5f3902ef3d3435c8c10106d6d2b
abematsu keisuke <sakura-jima@live.jp>
2022-10-24 02:36:43 +0900

    サーチ機能に取り掛かっています。

--- app/Http/Controllers/Api/ActorUserController.php
+++ app/Http/Controllers/Api/ActorUserController.php
@@ -84,20 +84,82 @@ class ActorUserController extends Controller
         ], 200);
     }
 
-    public function name_seach(Request $request)
+    public function name_search(Request $request)
+    {
+
+        if (!empty($request)) {
+
+            $search = play_condition::query()
+            ->where('$request->input("honban")','=','1')
+            ->orWhere('$request->input("gomunashi")','=','1')
+            ->orWhere('$request->input("nakadashi")','=','1')
+            ->orWhere('$request->input("ferachio")','=','1')
+            ->orWhere('$request->input("iramachio")','=','1')
+            ->orWhere('$request->input("kounaihassha")','=','1')
+            ->orWhere('$request->input("gansha")','=','1')
+            ->orWhere('$request->input("gokkun")','=','1')
+            ->orWhere('$request->input("bukkake")','=','1')
+            ->orWhere('$request->input("anal")','=','1')
+            ->orWhere('$request->input("anal_finger")','=','1')
+            ->orWhere('$request->input("anal_toy")','=','1')
+            ->orWhere('$request->input("anal_dankon")','=','1')
+            ->orWhere('$request->input("toys")','=','1')
+            ->orWhere('$request->input("rotar")','=','1')
+            ->orWhere('$request->input("denma")','=','1')
+            ->orWhere('$request->input("vibe")','=','1')
+            ->orWhere('$request->input("machine_vibe")','=','1')
+            ->orWhere('$request->input("chizyo")','=','1')
+            ->orWhere('$request->input("roshutsu")','=','1')
+            ->orwhere('$request->input("gaihakuroke")','=','1')
+            ->orwhere('$request->input("gaikokujin")','=','1')
+            ->orwhere('$request->input("les_tachi")','=','1')
+            ->orwhere('$request->input("les_neko")','=','1')
+            ->orwhere('$request->input("multiplay")','=','1')
+            ->orwhere('$request->input("onani")','=','1')
+            ->orwhere('$request->input("teimou")','=','1')
+            ->orwhere('$request->input("hounyou")','=','1')
+            ->orwhere('$request->input("innyou")','=','1')
+            ->orwhere('$request->input("yokunyou")','=','1')
+            ->orwhere('$request->input("giji_innyou")','=','1')
+            ->orwhere('$request->input("rape")','=','1')
+            ->orwhere('$request->input("rape_head")','=','1')
+            ->orwhere('$request->input("sm")','=','1')
+            ->orwhere('$request->input("spamking")','=','1')
+            ->orwhere('$request->input("bara_muchi")','=','1')
+            ->orwhere('$request->input("ippon_muchi")','=','1')
+            ->orwhere('$request->input("rousoku")','=','1')
+            ->orwhere('$request->input("kinbaku")','=','1')
+            ->orwhere('$request->input("hanahukku")','=','1')
+            ->orwhere('$request->input("kanchou")','=','1')
+            ->orwhere('$request->input("binta")','=','1')
+            ->orwhere('$request->input("kubisime")','=','1')
+            ->orwhere('$request->input("fist")','=','1')
+            ->orwhere('$request->input("dance")','=','1')
+            ->save();
+        }
+        return response()->json([
+            'message' => 'success',
+            'search' => $search,
+        ], 200);
+
+
+
+    }
+
+    public function condition_search(Request $request)
     {
 
     if (!empty($request)) {
         //検索キーワードのnameをnameとする場合
-        $name = $request;
-        $user_name = ActorUser::where('user_name','like' ,"%$name%")
+        $user_name = ActorUser::where('user_name', 'like', "%$name%")
         ->save();
+    }
         return response()->json([
             'message' => 'success',
             'user_name' => $user_name,
         ], 200);
 
 
-    }
+
     }
 }
